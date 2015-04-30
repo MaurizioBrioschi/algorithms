eb78d57b585c975e92e607ddaf736bd79854f660
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-30 10:06:24 +0200

    more test

--- Tests/QuickSortTest.php
+++ Tests/QuickSortTest.php
@@ -1,5 +1,6 @@
 <?php
 require('quicksort.php');
+ini_set('xdebug.max_nesting_level',10000);
 /**
  * Description of QuickSortTest
  *
@@ -10,5 +11,15 @@ class QuickSortTest extends PHPUnit_Framework_TestCase
     public function testQuickSort(){
         $array = [5,7,200,300,1,2,90,7,2000,69,50,30,9,11];
         $this->assertEquals([1,2,5,7,7,9,11,30,50,69,90,200,300,2000], quicksort($array));
+        $limit = 1000;
+        $array2 = [];
+        for($i=$limit;$i>0;$i--)  {
+            array_push($array2, $i);
+        }
+        $array2sorted = [];
+        for($i=1;$i<=$limit;$i++)    {
+            array_push($array2sorted,$i);
+        }
+        $this->assertEquals($array2sorted, quicksort($array2));
     }
 }
